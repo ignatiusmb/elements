4ea766a5539240ded8ee6ec1ad58c5be5e1f0d55
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-20 00:55:46 +0700

    Add Link element

---
+++ svelte/Link.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let href = '';
+	export let download = false;
+	export let newTab = false;
+	export let label = null;
+	export let inherit = false;
+	export let invert = false;
+</script>
+
+<a
+	{href}
+	{download}
+	target={newTab ? '_blank' : '_self'}
+	rel={href.includes('//') ? 'noopener noreferrer' : 'preload'}
+	aria-label={label}
+	class:disabled={!href}
+	class:inherit
+	class:invert>
+	<slot />
+</a>
+
+<style>
+	a {
+		cursor: pointer;
+	}
+	a.disabled {
+		cursor: not-allowed;
+	}
+	a.inherit {
+		color: inherit !important;
+	}
+	a.invert:hover {
+		filter: invert() hue-rotate(180deg);
+	}
+</style>
