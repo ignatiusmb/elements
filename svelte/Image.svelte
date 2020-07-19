7a46cd1d7c0b1e1bfe8acd14521d705ad10fc313
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-20 01:25:57 +0700

    Add Image and LazyLoad wrapper

---
+++ svelte/Image.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let src, alt;
+	export let absolute = false;
+
+	let loaded = false;
+	let image;
+
+	import { onMount } from 'svelte';
+	onMount(() => (image.onload = () => (loaded = true)));
+</script>
+
+<img {src} {alt} bind:this={image} class:loaded class:absolute />
+
+<style>
+	img {
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+		transition: opacity 1s ease-out;
+	}
+	img.loaded {
+		opacity: 1;
+	}
+	img.absolute {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+</style>
