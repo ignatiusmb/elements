c6745120534002b84028dc158aa157fe7ba6be4d
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-20 01:35:07 +0700

    Add main svelte input file

---
+++ svelte/index.js
@@ -0,0 +1,8 @@
+export { default as FeatherIcons } from './FeatherIcons.svelte';
+export { default as Icon } from './Icon.svelte';
+export { default as Image } from './Image.svelte';
+export { default as LazyLoadImage } from './LazyLoadImage.svelte';
+export { default as Link } from './Link.svelte';
+export { default as Observe } from './Observe.svelte';
+export { default as Pagination } from './Pagination.svelte';
+export { default as ProgressBar } from './ProgressBar.svelte';
