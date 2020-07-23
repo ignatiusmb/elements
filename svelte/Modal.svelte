7f2dab9ab6847c3646ce54dac9503c5427bd2bff
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-23 22:20:52 +0700

    Add Modal component

---
+++ svelte/Modal.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let show;
+	import { fly } from 'svelte/transition';
+	import { duration } from './options';
+</script>
+
+{#if show}
+	<div class="elements backdrop" on:click|self={() => (show = false)}>
+		<section class="elements modal-content" transition:fly={{ duration }}>
+			<slot>
+				<code>section.modal-content</code>
+			</slot>
+		</section>
+	</div>
+{/if}
+
+<style>
+	div {
+		z-index: 3;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		position: fixed;
+		display: grid;
+		align-items: center;
+		justify-content: center;
+		background: rgba(0, 0, 0, 0.8);
+	}
+	section {
+		padding: 2em;
+		border-radius: 0.5em;
+		background: #fff;
+	}
+</style>
