d6c17d977fa9f9341eec648e64c041a1737c14f0
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-25 20:10:57 +0700

    Feat: Add blog elements (#3)

---
+++ svelte/Overlay.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let show;
+	import { fade } from 'svelte/transition';
+	import { duration } from './options';
+</script>
+
+{#if show}
+	<div class="elements overlay" transition:fade={{ duration }}>
+		<slot />
+	</div>
+{/if}
+
+<style>
+	div {
+		z-index: 2;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		display: grid;
+		justify-items: center;
+		align-content: space-between;
+		padding: 1em 0.5em;
+		border-radius: inherit;
+		background-color: rgba(0, 0, 0, 0.7);
+		color: #fff;
+		text-align: center;
+	}
+</style>
