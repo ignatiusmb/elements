59eec7ff69d3d39fa872bc2af722d82833caf706
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-26 23:30:15 +0700

    Align alt text to center

--- svelte/Image.svelte
+++ svelte/Image.svelte
@@ -36,12 +36,13 @@
 		left: 0;
 	}
 	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
 		position: absolute;
 		top: 0;
 		left: 0;
-		width: 100%;
-		height: 100%;
 		border-radius: inherit;
-		object-fit: cover;
+		text-align: center;
 	}
 </style>
