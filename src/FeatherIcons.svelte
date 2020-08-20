3057663ec7e6034c5800d77af144988ea9a46d5c
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-08-20 14:12:33 +0700

    Add share icon

--- src/FeatherIcons.svelte
+++ src/FeatherIcons.svelte
@@ -229,6 +229,12 @@
 			<polyline points="7 3 7 8 15 8" />
 		</symbol>
 
+		<symbol id="share" viewBox="0 00 24 24">
+			<path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
+			<polyline points="16 6 12 2 8 6" />
+			<line x1="12" y1="2" x2="12" y2="15" />
+		</symbol>
+
 		<symbol id="sliders" viewBox="0 00 24 24">
 			<line x1="4" y1="21" x2="4" y2="14" />
 			<line x1="4" y1="10" x2="4" y2="3" />
