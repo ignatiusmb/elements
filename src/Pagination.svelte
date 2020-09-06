ea707a0cf3b5dcec8b014145f71062b2bddc6d7b
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-09-06 23:33:12 +0700

    Fix curr value and add comp value

--- src/Pagination.svelte
+++ src/Pagination.svelte
@@ -4,22 +4,24 @@
 	export let total = 0;
 	export let bound = 1;
 	import { ChevronsLeft, ChevronLeft, ChevronsRight, ChevronRight } from './icons';
-	function setPage(index) {
+	function update(index) {
 		if (index < 0) return;
 		index = Math.round(index);
-		if (index > $store && curr + bound > total) return;
+		if (index < $store) $store = index;
+		else if (comp > total) return;
 		$store = index;
 	}
-	$: curr = $store * bound;
-	$: next = curr + bound <= total ? curr + bound : total;
+	$: curr = $store * bound + 1;
+	$: comp = curr - 1 + bound;
+	$: next = comp <= total ? comp : total;
 	$: limit = Math.floor(total / bound);
 </script>
 
 <section class="lmns lmns-pagination">
-	<span class:disabled={$store === 0} on:click|preventDefault={() => setPage(0)}>
+	<span class:disabled={$store === 0} on:click|preventDefault={() => update(0)}>
 		<ChevronsLeft />
 	</span>
-	<span class:disabled={$store === 0} on:click|preventDefault={() => setPage($store - 1)}>
+	<span class:disabled={$store === 0} on:click|preventDefault={() => update($store - 1)}>
 		<ChevronLeft />
 	</span>
 
@@ -27,10 +29,10 @@
 		<div>{curr} - {next} / {total}</div>
 	</slot>
 
-	<span class:disabled={$store === limit} on:click|preventDefault={() => setPage($store + 1)}>
+	<span class:disabled={$store === limit} on:click|preventDefault={() => update($store + 1)}>
 		<ChevronRight />
 	</span>
-	<span class:disabled={$store === limit} on:click|preventDefault={() => setPage(limit)}>
+	<span class:disabled={$store === limit} on:click|preventDefault={() => update(limit)}>
 		<ChevronsRight />
 	</span>
 </section>
