f429ab92be80bed0d4b3bcffc20b4fde5007e6c6
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-09-12 04:51:44 +0700

    Add other Image props

--- src/LazyLoadImage.svelte
+++ src/LazyLoadImage.svelte
@@ -1,8 +1,10 @@
 <script>
 	export let src = '';
 	export let alt = '';
+	export let contain = false;
 	export let overlay = false;
 	export let absolute = false;
+	export let ratio = 9 / 16;
 
 	import Observe from './Observe.svelte';
 	import Image from './Image.svelte';
@@ -10,7 +12,7 @@
 
 <Observe once let:sighted>
 	{#if sighted}
-		<Image {alt} {src} {overlay} {absolute}>
+		<Image {alt} {src} {contain} {overlay} {absolute} {ratio}>
 			{#if overlay}
 				<slot />
 			{/if}
