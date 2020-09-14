56f6f03b23fd5689d12360c49664bb59cc9d8fa3
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-09-14 23:22:13 +0700

    Add main file and App entry point

---
+++ App.svelte
@@ -0,0 +1,14 @@
+<script>
+	// import { Component } from './src';
+</script>
+
+<!-- <Component /> -->
+
+<style>
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
